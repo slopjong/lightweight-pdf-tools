061011a1200c4d0b0bc2dd7b12254d50e96bbfe5
slopjong <slopjong>
2011-11-12 02:14:05 +0100

    Renamed back some scripts to avoid a conflict with pdfjam's pdfjoin and modified the Concat source slightly. Improved the build file to better support other platforms (windows, ...)

--- src/Concat.java
+++ src/Concat.java
@@ -37,20 +37,24 @@ public class Concat {
 	System.out.println("Test");
 	    
         if (args.length < 2) {
-            System.err.println("arguments: destfile file1 [file2 ...]");
+            System.err.println("arguments: file1 [file2 ...] destfile");
         }
         else {
             try {
                 int pageOffset = 0;
                 ArrayList master = new ArrayList();
-                int f =1;
-                String outFile = args[0];
+                
+                String outFile = args[args.length-1];
                 Document document = null;
                 PdfCopy  writer = null;
-                while (f < args.length) {
+                
+                //*
+                for(int f=0; f < args.length-1; f++) {
+                
                     // we create a reader for a certain document
                     PdfReader reader = new PdfReader(args[f]);
                     reader.consolidateNamedDestinations();
+                    
                     // we retrieve the total number of pages
                     int n = reader.getNumberOfPages();
                     List bookmarks = SimpleBookmark.getBookmark(reader);
@@ -62,7 +66,7 @@ public class Concat {
                     pageOffset += n;
                     System.out.println("There are " + n + " pages in " + args[f]);
                     
-                    if (f == 1) {
+                    if (f == 0) {
                         // step 1: creation of a document-object
                         document = new Document(reader.getPageSizeWithRotation(1));
                         // step 2: we create a writer that listens to the document
@@ -70,6 +74,7 @@ public class Concat {
                         // step 3: we open the document
                         document.open();
                     }
+                    
                     // step 4: we add content
                     PdfImportedPage page;
                     for (int i = 0; i < n; ) {
@@ -78,15 +83,18 @@ public class Concat {
                         writer.addPage(page);
                         System.out.println("Processed page " + i);
                     }
+                    
                     PRAcroForm form = reader.getAcroForm();
                     if (form != null)
                         writer.copyAcroForm(reader);
-                    f++;
                 }
+                
                 if (master.size() > 0)
                     writer.setOutlines(master);
+                    
                 // step 5: we close the document
                 document.close();
+                //*/
             }
             catch(Exception e) {
                 e.printStackTrace();
